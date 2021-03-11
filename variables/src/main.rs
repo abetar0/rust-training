37f48ad10d9f1641d85236c32c2e60345e74f2df
Taro Abe <tarofuku827@gmail.com>
2021-03-11 22:49:05 +0900

    ?

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let mut x = 5;
     let mut y = "fddd";
-    let mut z = "kkk";
+    let mut _z = "kkk";
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+    y = "fddd";
 }
